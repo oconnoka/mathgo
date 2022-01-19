23f9cb31036e30f487a5e700609fc26adb413f7e
Kathleen O'Connor <oconnoka@oregonstate.edu>
2022-01-19 17:37:55 -0500

    Initial Commit

---
+++ Math Go/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Math Go
+//
+//  Created by Kathleen O'Connor on 1/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
