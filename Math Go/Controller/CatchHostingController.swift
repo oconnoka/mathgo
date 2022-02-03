30fd8c2ce3991d13a5ec171d85448e9bc55f08e0
lfbettcher <57979763+lfbettcher@users.noreply.github.com>
2022-02-02 21:33:04 -0800

    Catch Beastie Screen View

---
+++ Math Go/Controller/CatchHostingController.swift
@@ -0,0 +1,12 @@
+import UIKit
+import SwiftUI
+
+class CatchHostingController: UIHostingController<CatchView> {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+        
+}
