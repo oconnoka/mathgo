089372af40dff086687578ffd09388fa2e371878
Kathleen O'Connor <oconnoka@oregonstate.edu>
2022-02-12 20:10:07 -0500

    Profile Screen - deleted dropdown menu for avatar selection in profile, deintegrated Cocoapods

--- Math Go/Controller/ProfileViewController.swift
+++ Math Go/Controller/ProfileViewController.swift
@@ -7,41 +7,11 @@
 
 import UIKit
 import SwiftUI
-import DropDown
 
 class ProfileViewController: UIViewController {
     @IBOutlet weak var selectAvatar: UIButton!
     @IBOutlet weak var playerName: UILabel!
     @IBOutlet weak var playerBeastieCount: UILabel!
-    @IBOutlet var playerAvatar: UIImageView!
-    
-    let menu: DropDown = {
-        let menu = DropDown()
-        menu.dataSource = [
-            "Image1",
-            "Image2",
-            "Image3",
-            "Image4",
-            "Image5"
-        ]
-        
-        let avatars = [
-            UIImage(named: "avatar1.png"),
-            UIImage(named: "avatar2.png"),
-            UIImage(named: "avatar3.png"),
-            UIImage(named: "avatar4.png"),
-            UIImage(named: "avatar5.png")
-        ]
-        
-        menu.cellNib = UINib(nibName: "DropDownCell", bundle: nil)
-        menu.customCellConfiguration = { index, title, cell in
-            guard let cell =  cell as? MyCell else {
-                return
-            }
-            cell.myImageView.image = avatars[index]
-        }
-        return menu
-    }()
 
     let appDelegate = UIApplication.shared.delegate as! AppDelegate
 
@@ -54,16 +24,6 @@ class ProfileViewController: UIViewController {
         let numberOfBeasties = appDelegate.player.beasties.count
         playerBeastieCount.text = "\(numberOfBeasties)"
         
-        menu.anchorView = selectAvatar
-        
-        menu.selectionAction = { index, title in
-            print("index \(index) and \(title)")
-        }
-        
-    }
-
-    @IBAction func showAvatarOption(_ sender:Any){
-        menu.show()
     }
      
     @IBAction func showAlert() {
